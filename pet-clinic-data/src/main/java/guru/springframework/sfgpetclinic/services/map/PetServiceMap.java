16df052e0fd1dac4415fa024a3a576890a545296
xaqshnar <pai.divya03@gmail.com>
2020-03-29 17:34:50 +0530

    Implement mad based services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+	@Override
+	public Set<Pet> finaAll(){
+
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
