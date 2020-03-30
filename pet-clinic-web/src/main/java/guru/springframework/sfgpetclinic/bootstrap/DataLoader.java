99088606ea0d164cb63f7b2e85266c1d9579c1eb
xaqshnar <pai.divya03@gmail.com>
2020-03-30 15:15:23 +0530

    Added auto increment of IDs for vets and owners list page Changed the abstract map service and owner and vet service map

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -25,25 +25,21 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Mark");
 		owner1.setLastName("Plier");		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Muy");
 		owner2.setLastName("Skerm");
 		ownerService.save(owner2);
 
 		Owner owner3 = new Owner();
-		owner3.setId(3L);
 		owner3.setFirstName("Jack");
 		owner3.setLastName("Septiceye");
 		ownerService.save(owner3);
 		
 		Owner owner4 = new Owner();
-		owner4.setId(4L);
 		owner4.setFirstName("Lord");
 		owner4.setLastName("Minion777");
 		ownerService.save(owner4);
@@ -51,13 +47,11 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Added owner");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Cream");
 		vet1.setLastName("Hero");
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Ari");
 		vet2.setLastName("Rirang");
 		vetService.save(vet2);		
